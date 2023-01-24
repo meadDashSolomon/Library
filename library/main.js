6fd60d4bdc873d2a2b64516687b09bd6bd72cbd6
Mead Solomon <mead93@gmail.com>
2023-01-24 15:50:31 -0600

    filled in myLibrary const's array. created createBookElement, createBookItem, and renderBooks functions

--- library/main.js
+++ library/main.js
@@ -1,2 +1,39 @@
 const books = document.querySelector(".books");
-const myLibrary = [];
+
+const myLibrary = [
+  {
+    title: "Book1",
+    author: "me",
+    pages: "500",
+    read: true,
+  },
+  {
+    title: "Book2",
+    author: "you",
+    pages: "5000",
+    read: false,
+  },
+];
+
+function createBookElement(el, content, className) {
+  const element = document.createElement(element);
+  element.textContent = content;
+  element.setAttribute("class", className);
+  return element;
+}
+
+function createBookItem(book, index) {
+  const bookItem = document.createElement("div");
+  bookItem.setAttribute("id", index);
+  bookItem.setAttribute("key", index);
+  bookItem.setAttribute("class", "card book");
+  bookItem.appendChild(
+    creatBookElement("h1", `Title: ${book.title}`, "book-title")
+  );
+}
+
+function renderBooks() {
+  myLibrary.map((book, index) => {
+    creatBookItem(book, index);
+  });
+}
